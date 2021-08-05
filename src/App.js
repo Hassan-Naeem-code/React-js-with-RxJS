c318197646c1776abb388c013bda41bb40977996
Hassan-Naeem-code <nhassan_1990@yahoo.com>
2021-08-05 23:31:35 +0500

    some operator and method are been tested from rxjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { of, interval, concat, Subject } from "rxjs";
+import {
+  takeWhile,
+  takeUntil,
+  scan,
+  startWith,
+  repeatWhen,
+  share,
+} from "rxjs/operators";
 
-function App() {
+const observable = interval(250).pipe(
+  startWith(5),
+  scan(time=>time - 1),
+  takeWhile(time=> time > 0)
+);
+
+const result = concat(observable,of('Wake Up!'));
+const action = new Subject();
+action.subscribe(console.log('Hello World'));
+
+export const App = () => {
+  const [state, setstate] = useState();
+  useEffect(() => {
+    const listener = result.subscribe(setstate);
+    return ()=> listener.unsubscribe();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Alarm Clock </h1>
+      <h1>{state} </h1>
+      <button onClick={()=>{action.next('snooze')}}>Snooze</button>
     </div>
   );
-}
+};
 
 export default App;
